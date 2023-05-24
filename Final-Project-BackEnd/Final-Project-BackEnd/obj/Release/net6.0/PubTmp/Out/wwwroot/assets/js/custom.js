1e4e3c6d825345c562d4048a0a5d24a66293814e
gulshanfm <gulshanfm@code.edu.az>
2023-05-24 12:32:48 +0400

    publish

---
+++ Final-Project-BackEnd/Final-Project-BackEnd/obj/Release/net6.0/PubTmp/Out/wwwroot/assets/js/custom.js
@@ -0,0 +1,121 @@
+﻿$(document).ready(function () {
+    
+
+        
+    $(document).on('click', '.addToBasket', (function () {
+        let productId = $(this).data('id');
+        console.log(productId)
+        fetch('/basket/AddBasket?id=' + productId)
+            .then(res => {
+                return res.text();
+            }).then(data => {
+
+                $('.header-cart').html(data);
+                $(".basket .basket-icon").click(function () {
+                    $(".basket-content").toggle("d-block");
+                    $(".basket-content .close-div").click(function () {
+                        $(".basket-content").css({ "display": "none" });
+                        console.log("hhwllo");
+                    })
+                });
+            })
+
+    }))
+    //DeCrease And InCrease Count
+    $(document).on('click', '.plus-btn', function () {
+
+        let productId = $(this).attr('data-id')
+        console.log(productId)
+        fetch("/basket/IncreaseCount?productId=" + productId)
+            .then(res => {
+                return res.text();
+            })
+            .then(data => {
+                $('.cart-area').html(data)
+            });
+
+    })
+
+    $(document).on('click', '.minus-btn', function () {
+
+        let productId = $(this).attr('data-id')
+        console.log(productId)
+        fetch("/basket/DecreaseCount?productId=" + productId)
+            .then(res => {
+                return res.text();
+            })
+            .then(data => {
+                $('.cart-area').html(data)
+            });
+
+
+
+    })
+
+
+    $(document).on('click', '.removeProdInCart', function () {
+
+
+        let productId = $(this).attr('data-id');
+        console.log(productId)
+        fetch("/basket/DeleteCart?id=" + productId).then(res => {
+            return res.text();
+        })
+            .then(data => {
+                $('.cart-area').html(data);
+            })
+    })
+
+
+
+    //$('.addToBasket').click(function myfunction(e) {
+    //    e.preventDefault();
+    //    console.log("hhjjjjjjjjj");
+    //    let productId = $(this).data('id');
+
+    //    fetch('/basket/AddBasket?id=' + productId)
+    //        .then(res => {
+    //            return res.text();
+    //        }).then(data => {
+
+    //            $('.header-cart').html(data);
+    //        })
+    //})
+
+    $(document).on('click', '.addAddress', function (e) {
+        e.preventDefault();
+
+        $('.addressContainer').addClass('d-none');
+        $('.addressForm').removeClass('d-none');
+    })
+})
+
+$('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true
+});
+
+$(document).on('click', '.secondImageDetail', (function () {
+    let secondImgUrl = $(this).attr('src')
+    let mainImageUrl = $('.mainImageDetail').attr('src')
+    let mainBgImage = $('.mainImageDetail')
+    console.log(secondImgUrl)
+    console.log(mainImageUrl)
+    $('.mainImageDetail').attr('src') = secondImgUrl
+    $(this).attr('src') = mainImageUrl
+
+
+
+
+}))
---
+++ Final-Project-BackEnd/Final-Project-BackEnd/obj/Release/net6.0/PubTmp/Out/wwwroot/manage/js/custom.js
@@ -0,0 +1,87 @@
+﻿$(document).ready(function () {
+    $(document).on('click', '.updateBtn', function (e) {
+        e.preventDefault();
+
+        let parent = $(this).parent();
+        let prev = parent.prev();
+        parent.addClass('d-none');
+        prev.addClass('d-none');
+
+        let next = parent.next();
+        next.removeClass('d-none');
+    })
+
+
+    $(document).on('click', '.deleteBtn', function (e) {
+        e.preventDefault();
+
+        let url = $(this).attr('href');
+
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(url)
+                    .then(res => res.text())
+                    .then(data => {
+                        $('.indexContainer').html(data)
+                    })
+                Swal.fire(
+                    'Deleted!',
+                    'Your file has been deleted.',
+                    'success'
+                )
+            }
+        })
+    })
+
+    $(document).on('click', '.deleteImage', function (e) {
+        e.preventDefault();
+
+        let url = $('.deleteImage').attr('href');
+        let imageId = $(this).attr('data-imageId');
+
+        fetch(url + "?imageId=" + imageId)
+            .then(res => {
+                if (res.ok) {
+                    return res.text()
+                } else {
+                    alert("Yanlis Emeliyyat");
+                    return;
+                }
+            })
+            .then(data => {
+                $('.productImages').html(data)
+            })
+
+        //console.log(url + "?imageId=" + imageId);
+    })
+
+    let isMain = $('#IsMain').is(':checked');
+
+    if (isMain) {
+        $('#fileInput').removeClass('d-none');
+        $('#parentList').addClass('d-none');
+    } else {
+        $('#fileInput').addClass('d-none');
+        $('#parentList').removeClass('d-none');
+    }
+
+    $('#IsMain').click(function () {
+        let isMain = $(this).is(':checked');
+
+        if (isMain) {
+            $('#fileInput').removeClass('d-none');
+            $('#parentList').addClass('d-none');
+        } else {
+            $('#fileInput').addClass('d-none');
+            $('#parentList').removeClass('d-none');
+        }
+    })
+})
